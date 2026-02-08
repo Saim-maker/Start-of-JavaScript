c721842670a73850a2be6b103cbb37ba2da7f732
Saim-maker <saaimnawaz2@gmail.com>
2026-02-08 20:05:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Practice Questions? (Part 1)
+
+console.log(null + 1);
+console.log("5" + 3);
+console.log("5" - 3);
+console.log(true + false);
+
+//Check Types?
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof 123n);
